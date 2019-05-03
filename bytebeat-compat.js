a0826d0d3f855ab946c98fb2a2084a79d4b44d2f
Danny Robinson <dannyrobinson@gmx.com>
2019-05-03 20:56:31 +0100

    Compatibility check changed to IIFE

--- bytebeat-compat.js
+++ bytebeat-compat.js
@@ -1,5 +1,3 @@
-let newsrc = document.createElement('script')
-newsrc.setAttribute('src', window.AudioWorkletNode ? 'bytebeat.js' : 'bytebeat-noworklets.js')
-
-document.head.replaceChild(newsrc,
-  document.head.querySelector('[src="bytebeat-compat.js"]'))
+;(()=>{let n=document.createElement('script')
+n.setAttribute('src',window.AudioWorkletNode?'bytebeat.js':'bytebeat-noworklets.js')
+document.head.replaceChild(n,document.currentScript)})()
