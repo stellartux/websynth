19f643404a2acfc30d230baebe7e161d91cf0960
Danny Robinson <dannyrobinson@gmx.com>
2020-04-01 07:49:06 +0100

    Compile RPN string to Wasm, with tests

--- src/rpn.js
+++ src/rpn.js
@@ -1,3 +1,5 @@
+import { LEB128 } from './leb128.js'
+
 /**
  * Functions for interpreting Reverse Polish Notation code
  * @namespace RPN
@@ -334,7 +336,147 @@ export const RPN = {
   isValidGlitchURL: function(url) {
     return /^(glitch:\/\/)?[^!]*![a-hj-u!A-F\d.]+$/.test(url)
   },
+  glitchToWat: {
+    '+': { text: 'i32.add', opcode: [0x6a] },
+    '-': { text: 'i32.sub', opcode: [0x6b] },
+    '*': { text: 'i32.mul', opcode: [0x6c] },
+    '/': { text: 'i32.div_u', opcode: [0x6e] },
+    '%': { text: 'i32.rem_u', opcode: [0x70] },
+    '~': { text: 'i32.const -1\ni32.xor', opcode: [0x41, 0x7f, 0x73] },
+    '>>': { text: 'i32.shr_s', opcode: [0x75] },
+    '>>>': { text: 'i32.shr_u', opcode: [0x76] },
+    '<<': { text: 'i32.shl', opcode: [0x74] },
+    '&': { text: 'i32.and', opcode: [0x71] },
+    '|': { text: 'i32.or', opcode: [0x72] },
+    '^': { text: 'i32.xor', opcode: [0x73] },
+    '=': { text: 'i32.eq', opcode: [0x46] },
+    '>': { text: 'i32.gt_u', opcode: [0x4b] },
+    '<': { text: 'i32.lt_u', opcode: [0x49] },
+    drop: { text: 'drop', opcode: [0x1a] },
+    // TODO: swap, dup, pick, put
+    t: { text: 'local.get $t', opcode: [0x20, 0x0] },
+    tt: { text: 'local.get $tt', opcode: [0x20, 0x1] },
+  },
+  toWat: function(code) {
+    return this.wrapWat(this.tokensToWat(code))
+  },
+  tokensToWat: function(code) {
+    return this.desugar(code)
+      .split(/\s+/)
+      .map(token => {
+        if (token in this.glitchToWat) {
+          return this.glitchToWat[token].text
+        } else if (/^\d+$/.test(token)) {
+          return `i32.const ${token}`
+        } else {
+          throw Error(`Could not parse ${token}`)
+        }
+      })
+      .join('\n')
+  },
+  wrapWat: function(code) {
+    return `(module (type $t0 (func (param i32 i32) (result i32))) (func $bytebeat
+      (export "bytebeat") (type $t0) (param $t i32) (param $tt i32) (result i32)
+      ${code}))`
+  },
+  wasmHeader: [
+    0,
+    0x61,
+    0x73,
+    0x6d, // WASM_BINARY_MAGIC
+    1,
+    0,
+    0,
+    0, // WASM_BINARY_VERSION
+    // section "Type" (1)
+    1, // section code
+    7, // section size
+    1, // num types
+    0x60, // func
+    2, // num params
+    0x7f, // i32
+    0x7f, // i32
+    1, // num results
+    0x7f, // i32
+    // section "Function" (3)
+    3, // section code
+    2, // section size
+    1, // num functions
+    0, // function 0 signature index
+    // section "Export" (7)
+    7, // section code
+    0xc, // section size
+    1, // num exports
+    8, // string length
+    0x62,
+    0x79,
+    0x74,
+    0x65,
+    0x62,
+    0x65,
+    0x61,
+    0x74, // 'bytebeat'
+    0, // export kind
+    0, // export func index
+    0xa, // section code
+  ],
+  wasmFooter: [
+    0, // section code
+    0x1e, // section size
+    4, // string length
+    0x6e,
+    0x61,
+    0x6d,
+    0x65, // custom section name
+    1, // function name type
+    0xb, // subsection size (guess)
+    1, // num functions
+    0, // function index
+    8, // string length
+    0x62,
+    0x79,
+    0x74,
+    0x65,
+    0x62,
+    0x65,
+    0x61,
+    0x74, // "bytebeat" func name 0
+    2, // local name type
+    0xa, // subsection size
+    1, // num functions
+    0, // function index
+    2, // num locals
+    0, // local index
+    1, // string length
+    0x74, // t : local name 0
+    1, // local index
+    2, // string length
+    0x74,
+    0x74, // tt : local name 1
+  ],
+  toWasmBinary: function(code) {
+    let func = [0] // local decl count = 0
+    for (const token of this.desugar(code).split(/\s+/g)) {
+      if (/\d+/.test(token)) {
+        func = func.concat(0x41, ...new LEB128(token))
+      } else if (token in this.glitchToWat) {
+        func = func.concat(...this.glitchToWat[token].opcode)
+      } else {
+        throw Error(`Could not understand ${token}`)
+      }
+    }
+    func.push(0xb)
+    const funcLen = new LEB128(func.length)
+
+    return new Uint8Array([
+      ...this.wasmHeader,
+      ...new LEB128(func.length + funcLen.length + 1),
+      1,
+      ...funcLen,
+      ...func,
+      ...this.wasmFooter
+    ])
+  },
 }
-RPN.glitchInterpret('1')
 
 export { RPN as default }
