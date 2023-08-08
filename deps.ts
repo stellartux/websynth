b1ab0b872d464e45bbd2a310fd7df55516561e38
Danny Robinson <dannyrobinson@gmx.com>
2023-08-08 17:58:58 +0100

    Remove JSdoc pages

--- deps.ts
+++ deps.ts
@@ -1,5 +1,1 @@
 export * from 'https://deno.land/std/testing/asserts.ts'
-export { bench, runBenchmarks } from 'https://deno.land/std/testing/bench.ts'
-export function runIfMain(meta: any): void {
-  if (meta.main) Deno.runTests()
-}
